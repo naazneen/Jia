f755be79812a8c724982e3da280aeda4a31bee5a
Naazneen Jatu <30753586+naazneen@users.noreply.github.com>
2018-09-17 19:23:57 +0530

    Add files via upload

--- functs.py
+++ functs.py
@@ -4,20 +4,33 @@ from selenium.webdriver.common.keys import Keys
 import speech_recognition as sr
 import sqlite3
 import re
+from Tkinter import *
+import datetime
 
 engine = pyttsx.init()
 engine.setProperty('rate', 110)
 con = sqlite3.connect('dbva.db')
 con.text_factory = str
 c = con.cursor()
+ 
+
 
 #functions
+def intro():
+    myname="JIyA"
+    time=datetime.datetime.now()
+    greet="Peace unto you."
+    engine.say("My name is "+myname+greet)
+    engine.say("Today it is " + time.strftime("%d %b"))
+    engine.runAndWait()
+    
 def fruits():
     voices = ('banana','apple','mango','Cherry ','Date' ,'Guava')
     engine.say("Fruits I know are")
     for f in voices:
         engine.say(f)
     engine.runAndWait()
+    return voices
 def Alpha():
     for row in c.execute("select letters from alpha"):
         engine.say(row)
@@ -34,21 +47,47 @@ def clr():
 
 def search(x):
     browser = webdriver.Chrome('C:\Users\ABDUL\Downloads\chromedriver_win32\chromedriver')
+    browser.minimize_window()
     browser.get('http://google.co.in/')
     sbar = browser.find_element_by_id('lst-ib')
     sbar.send_keys(x)
     sbar.send_keys(Keys.ENTER)
     
-    chkwhen = re.match(r'when',x)
-    if chkwhen:
-        elem = browser.find_element_by_css_selector('div.MUxGbd.t51gnb.lyLwlc.lEBKkf')
-    chkwhat = re.match(r'what',x)
-    if chkwhat:
+    try:
+        elem=browser.find_element_by_css_selector('div.MUxGbd.t51gnb.lyLwlc.lEBKkf')
+    except:
+        pass
+    try:
         elem = browser.find_element_by_css_selector('span.ILfuVd.yZ8quc')
-    chkwho = re.match(r'who',x)
-    if chkwho:
+    except:
+        pass
+    try:
         elem = browser.find_element_by_css_selector('div.Z0LcW')
+    except:
+        pass
+    try:
+        elem = browser.find_element_by_css_selector('div.title')
+    except:
+        pass
+    try:
+        elem = browser.find_element_by_css_selector('div.BbbuR.uc9Qxb.uE1RRc')
+    except:
+        pass
+    try:
+        elem = browser.find_element_by_css_selector('span.vk_gy.vk_sh')
+    except:
+        pass
+    
         
-    engine.say(elem.text)
-    engine.runAndWait()
+    elem = elem.text
+    elem = elem[:47]
+    return elem
+    #print (elem)
+    
+    
+
+def my(a):
+    r=c.execute("select value from MyData where key=a")
+    engine.say(r)
+
 
--- main.py
+++ main.py
@@ -1,48 +1,82 @@
 #imported stuff
-import datetime
 import pyttsx
 import speech_recognition as sr
-import sqlite3
 import functs as f
-
+import Tkinter as tk
+import thread
+from PIL import ImageTk, Image
 r = sr.Recognizer()
+
  
-#few declarations
-myname="JIyA"
-x="hi"
-time=datetime.datetime.now()
-greet="Peace to you. Today it is " + time.strftime("%d %b")
+#initialisation
 engine = pyttsx.init()
 engine.setProperty('rate', 110)
+x='h'
 
-def run():
-    engine.say("Hi. My name is "+ myname)
-    engine.say(greet)
-    engine.runAndWait()
+
+
+
 
 def sun():
-    with sr.Microphone() as source:
-            print ('Say Something!')
+    try:
+        with sr.Microphone() as source:
+            l1.configure(text="I'm Listening")
             audio = r.listen(source)
-            print ('Heard!')
             a = r.recognize_google(audio)
             return a
+    except:
+        l1.configure(text="Sorry. I didn't heard that.")
+        engine.say("Sorry. I didn't heard that.")
+        engine.runAndWait()
+        t2()
 
-#run()
-while(x!="bye"):
-    x=sun()
-    print(x)
-    if (x=="name some fruits"):
-        f.fruits()
-    elif (x=="read alphabets"):
-        f.Alpha()
-    elif (x=="what colours you know"):
-        f.clr()
-    elif (x=="days of the week"):
-        f.wdays()
-    elif (x!='bye'):
-        f.search(x)
-    else:
-        engine.say("Bye.")
-        
-engine.runAndWait()
+def bol():
+    x='hi'
+    while(True):
+        x=sun()
+        l2.config(text=x)
+        if (x=="who are you"):
+            f.intro()   
+        elif(x=="who is your best friend"):
+            engine.say("you are my best friend, ma'am")
+            engine.runAndWait()
+        elif (x=="name some fruits"):
+            y=f.fruits()
+            l1.config(text=y)
+        elif (x=="read alphabets"):
+            f.Alpha()
+        elif (x=="what colours you know"):
+            f.clr()
+        elif (x=="days of the week"):
+            f.wdays()
+        elif (x=="hi"):
+            f.intro()
+        elif (x=="bye"):
+            engine.say("Bye.")
+            l1.config(text="bye")
+            engine.runAndWait()
+            return
+        else:
+            a=f.search(x)
+            l1.config(text=a)
+            engine.say(a)
+            engine.runAndWait()
+def t2():
+    try:
+        thread.start_new_thread(bol,())
+    except:
+        print("error occured")
+
+#engine.runAndWait()
+root = tk.Tk()
+img = ImageTk.PhotoImage(Image.open("jiaaa.jpg"))
+panel = tk.Label(root, image = img)
+panel.pack()
+b1=tk.Button(root,text="Ask Jia",command=t2, background="#ffb3d9", foreground="#99004d",font=('Century Gothic',10))
+b1.pack()
+l1=tk.Label(root,text="Hey")
+l1.pack()
+l2=tk.Label(root,text="")
+l2.pack()
+root.mainloop()
+
