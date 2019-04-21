7e7231ee35b46b426f45c863a1bf7ed2af6078f8
Naazneen Jatu <30753586+naazneen@users.noreply.github.com>
2019-04-21 22:47:38 +0530

    Add files via upload

--- main_fresh.py
+++ main_fresh.py
@@ -13,6 +13,8 @@ import msvcrt
 import ttk
 import os
 import re
+import datetime
+
  
 #initialisation
 r = sr.Recognizer()
@@ -27,33 +29,71 @@ thank_responses=["You're welcome","always welcome ","my pleasure ","most welcome
 nonetwork=["Make sure you have internet connection ","Seems like internet is not available ","Check Internet connection I won't be able to help you "]
 notheardwords=["Sorry, I didn't hear you","I beg your pardon","Sorry, Can you please repeat that."]
 nomicwords=["Microphone is not detected."]
+yes=['yes','yeah','yess','yah','haa','sure']
+no=['no','nah','not','never','nahi']
 passwordd=""
 useraffix="Ma'am"
 user=["Ma'am","Madam","Boss"]
 
+#database
+conn = sqlite3.connect('dbva.db')
+conn.text_factory = str
+
+create_notes = """
+create table if not exists notes(nid integer primary key autoincrement,
+text varchar(150),date_created text,locked int);
+"""
+create_reminders = """
+create table if not exists reminders(rid integer primary key autoincrement,
+text varchar(150),issue_datetime text,reminder_datetime text, locked int);
+"""
+
+create_reports = """
+create table if not exists reports(repid integer primary key autoincrement,
+title varchar(150),length integer,issue_datetime text,
+due_datetime text, status text,viewed int,locked int);
+"""
+
+create_user = """
+create table if not exists user(key text,value text, locked int);
+"""
+
+#fun to create a table from the create_table_sql statement
+def create_table(conn, create_table_sql):
+    c = conn.cursor()
+    c.execute(create_table_sql)
+    print "table created"
+    
+
+create_table(conn,create_notes)
+create_table(conn,create_reminders)
+create_table(conn,create_reports)
+create_table(conn,create_user)
+
+#Functions
 
 def saybye():
-    res=random.choice(farewellwords)
+    res=random.choice(farewellwords)+" "
     res=res+random.choice(user)
     return res
 
 def greet():
-    res=random.choice(greetwords)
+    res=random.choice(greetwords)+" "
     res=res+random.choice(user)
     return res
 
 def thanks():
-    res=random.choice(thank_responses)
+    res=random.choice(thank_responses)+" "
     res=res+random.choice(user)
     return res
 
 def internetError():
-    res=random.choice(nonetwork)
+    res=random.choice(nonetwork)+" "
     res=res+random.choice(user)
     return res
 
 def notheard():
-    res=random.choice(notheardwords)
+    res=random.choice(notheardwords)+" "
     res=res+random.choice(user)
     return res
 
@@ -63,6 +103,176 @@ def nomic():
     res=res+" Attach Microphone andtry again."
     return res
 
+def what(x):
+    what = re.search('that',x)
+    if what==None:
+        what = re.search('to',x)
+    if what == None:
+        engine.say("What is it about?")
+        response.configure(text="What is it about?")
+        engine.runAndWait()
+        what = sun()
+        return what  
+    what = what.span()
+    z1 = what[1]
+    what = x[z1+1:]
+    return what
+
+def when(x):
+    on = re.search(' on',x)
+    if on != None:
+        on = on.span()
+        z1 = on[1]
+        at = re.search(' at',x)
+        after = re.search(' after',x)
+        if at == None:   
+            on = x[z1+1:]
+        elif at:
+            at = at.span()
+            z2 = at[1]
+            on = x[z1+1:z2-3]
+            print on
+        if after == None:   
+            on = x[z1+1:]
+        elif after:
+            after = after.span()
+            z2 = after[1]
+            on = x[z1+1:z2-6]
+            print on
+        if (on.isdigit()==True):
+            y = datetime.datetime.now().year
+            m = datetime.datetime.now().month
+            on = datetime.datetime.strptime(str(y)+"-"+str(m)+"-"+on,"%Y-%m-%d")
+            print on
+        else:
+            print "error"
+    if on == None:
+        on = datetime.date.today()
+    if re.search('tomorrow',x):
+        on = datetime.date.today() + datetime.timedelta(days=1)
+    
+    at = re.search(' at',x)
+    if at!=None:
+        at = at.span()
+        z2 = at[1]
+        at = x[z2+1:]
+     
+        at2 = re.sub(' a.m.','AM',at)
+    
+        at2 = re.sub(' p.m.','PM',at)
+
+        print at2
+        
+        at = datetime.datetime.strptime(at2,"%I:%M%p")
+        at.strftime("%H:%M:%S")
+        #print at
+        
+    elif (re.search('after',x)):
+        at = re.search('after',x)
+        at = at.span()
+        z2 = at[1]
+        at = x[z2+1:]
+        h = re.search('hours',at)
+        if h!=None:
+            h = h.span()
+            z3 = h[1]
+            print z3
+            h = at[:z3-5]
+            h = int(h)
+            at2 = datetime.datetime.now() + datetime.timedelta(hours=h)
+            format(at2,"%H:%M:%S")
+            """at2=str(at2)
+            at2 = at2[:19]
+            print at2"""
+        m = re.search('minutes',at)
+        if m!=None:
+            m = m.span()
+            z3 = m[1]
+            print z3
+            m = at[:z3-7]
+            if (re.search('hours',at)):
+                hi = re.search('hours',at)
+                hi = hi.span()
+                z4 = hi[1]
+                m = at[z4+1:z3-7]
+                m = int(m)
+                at3 = at2 + datetime.timedelta(minutes=m)
+                format(at3,"%H:%M:%S")
+                at3=str(at3)
+                at3 = at3[:19]
+                at = at3
+            else:
+                m = int(m)
+                at3 = datetime.datetime.now() + datetime.timedelta(minutes=m)
+                format(at3,"%H:%M:%S")
+                at3=str(at3)
+                at3 = at3[:19]
+                print at
+                at=at3
+    else:
+        engine.say("When should I remind?")
+        response.configure(text="When should I remind?")
+        engine.runAndWait()
+        whenq = sun()
+        return when(whenq) 
+
+    at=str(at)
+    at = at[10:]
+    on = str(on)
+    on = on[:10]   
+    on = on + at
+    on = datetime.datetime.strptime(on,"%Y-%m-%d %H:%M:%S")
+    return on.strftime("%Y-%m-%d %H:%M:%S")
+
+def makenote(x):
+    conn = sqlite3.connect('dbva.db')
+    conn.text_factory = str
+    item=[]
+    item.insert(0,what(x))
+    datecreated=datetime.datetime.now()
+    item.insert(1,datecreated.strftime("%Y-%m-%d %H:%M:%S"))
+    engine.say("Would you like to keep it private?")
+    response.configure(text="Would you like to keep it private?")
+    engine.runAndWait()
+    islocked = sun()
+    if islocked in yes:
+        islocked = 1
+    elif islocked in no:
+        islocked = 0
+    item.insert(2,islocked)
+    c = conn.cursor()
+    c.execute("insert into notes(text,date_created,locked) values(?,?,?)",item)
+    conn.commit()
+    conn.close()
+    engine.say("Note is created")
+    response.configure(text="Note is created")
+    engine.runAndWait()
+
+def makereminder(x):
+    conn = sqlite3.connect('dbva.db')
+    conn.text_factory = str
+    item=[]
+    item.insert(0,what(x))
+    datecreated=datetime.datetime.now()
+    item.insert(1,datecreated.strftime("%Y-%m-%d %H:%M:%S"))
+    item.insert(2,when(x))
+    engine.say("Would you like to keep it private?")
+    response.configure(text="Would you like to keep it private?")
+    engine.runAndWait()
+    islocked = sun()
+    if islocked in yes:
+        islocked = 1
+    elif islocked in no:
+        islocked = 0
+    item.insert(2,islocked)
+    c = conn.cursor()
+    c.execute("insert into reminders(text,issue_datetime,reminder_datetime,locked) values(?,?,?,?)",item)
+    conn.commit()
+    conn.close()
+    engine.say("Reminder is created")
+    response.configure(text="Reminder is created")
+    engine.runAndWait()
+    
 
 class App(threading.Thread):
     def __init__(self, tk_root):
@@ -95,14 +305,16 @@ def wake():
                     engine.runAndWait()
                     continue
                 else:
-                    logic(x)
+                    ex = logic(x)
+                    if ex == True:
+                        break
                     #thread.start_new_thread(logic,(x,))
                     #this was sooooooooo unnecessary!!!
-        if (re.search('jiya',x)):
+        """elif (re.search('jiya',x)):
             y = re.search('jiya',x).span()
             z = y[1]
             x = x[z+1:]
-            logic(x)
+            logic(x)"""
             
 
 def sleepy():
@@ -131,9 +343,8 @@ def sleepy():
         lbl.load('still1.jpg')
         print "inside except"
         return False
-    except Exception:
-        print Exception
-        return False
+    except:
+        pass
         """
         ierror=internetError()
         response.configure(text=ierror)
@@ -197,22 +408,22 @@ def logic(x):
     request.config(text=x)
     if (re.search('play',x)):
         threading.Thread(target=f.play,args=(x,)).start()
-        return
+        return True
     elif (re.search('find',x)):
         y=f.find(x)
         engine.say(y)
         response.config(text=y)
-    elif(re.search('open',x)):
+    elif (re.search('open',x)):
         y=f.find(x)
         os.startfile(y[0])
         engine.say("Here it is.")
         #engine.runAndWait()
-    elif (x=="name some fruits"):
-        y=f.fruits()
-        engine.say(y)
-        response.config(text=y)
-    elif (x=="read alphabets"):
-        f.Alpha()
+    elif (re.search('note',x)):
+        threading.Thread(target=makenote,args=(x,)).start()
+        return True
+    elif (re.search('remind',x) or re.search('reminder',x)):
+        threading.Thread(target=makereminder,args=(x,)).start()
+        return True
     elif (x=="what colours you know"):
         f.clr()
     elif (x=="days of the week"):
@@ -254,12 +465,94 @@ def showreports():
     frame3.grid_remove()
     frame5.grid_remove()
     frame4.grid(row=0,column=0,columnspan=3)
+    
+    frame41=tk.Frame(frame4,width=frame1["width"],height=267)
+    frame41.grid(row=0,column=0)
+    frame41.grid_propagate(0)
+
+    frame42=tk.Frame(frame4,width=frame1["width"])
+    frame42.grid(row=1,column=0,sticky="SW")
+
+    f4title=tk.Label(frame41,text="Reports",font=('Comic Sans',16))
+    f4title.grid(row=0,column=0,columnspan=2)
+
+    print "Opened database successfully";
+    cursor = conn.execute("SELECT * from reports")
+
+    i=1
+    j=0
+
+    def v_report(x):
+        print "report"
+        print x
+        
+
+    def v_details(x):
+        print "details"
+        x=str(x)
+        viewdetails = conn.execute("select * from reports where repid="+x)
+        for row in viewdetails:
+            r1=str(row[1])
+            r2=str(row[3])
+            r3=str(row[4])
+            r4=str(row[5])
+            t="Title : "+r1+"\nIssue Date : "+r2+"\nDue Date : "+r3+"\nStatus : "+r4
+            print t
+            dlabel=tk.Label(frame42,text=t,justify="left")
+            dlabel.grid(row=0,column=0)
+        
+
+    for row in cursor:
+        c = row[1]
+        b = tk.Label(frame41,relief="ridge", text=c,width=30,bg="white",padx=5,anchor="w")
+        b.grid(row=i, column=j,sticky="W",padx=5,pady=3)
+        report=tk.Button(frame41, text="report",bg="white", command = partial(v_report,row[0]))
+        report.grid(row=i,column=j+1,padx=5,pady=3)
+        detail=tk.Button(frame41, text="details",bg="white", command = partial(v_details,row[0]))
+        detail.grid(row=i,column=j+2,padx=5,pady=3)
+        i=i+1
+
 
 def shownotes():
     frame1.grid_remove() 
     frame3.grid_remove()
     frame4.grid_remove()
     frame5.grid(row=0,column=0,columnspan=3)
+    f5title=tk.Label(frame5,text="Notes",font=('Comic Sans',16),anchor="center")
+    f5title.grid(row=0,column=0,columnspan=3)
+    ncursor = conn.execute("SELECT * from notes")
+
+    def v_note(x):
+        response.configure(text="speak password")
+        engine.say("speak password")
+        engine.runAndWait()
+        global passwordd
+        p = sun()
+        if p == passwordd:
+            vnote= conn.execute("select text from notes where nid="+str(x))
+            for row in vnote:
+                t = row[0]
+                response.configure(text=t)
+                engine.say("Here is the note")
+                engine.runAndWait()
+        else:
+            response.configure(text="incorrect password")
+            engine.say("incorrect password")
+            engine.runAndWait()
+    ni=1
+    nj=-1
+    for row in ncursor:
+        nj+=1
+        if row[3]==1:
+            nc="Protected. Click to View"
+            nb = tk.Button(frame5,relief="ridge",wraplength=67,command = partial(v_note,row[0]),justify="left",text=nc,width=10,height=5,anchor="nw",bg="white") 
+        elif row[3]==0:    
+            nc = row[1]
+            nb = tk.Label(frame5,relief="ridge",wraplength=67,justify="left",text=nc,width=10,height=5,anchor="nw",bg="white")
+        if nj==3:
+            ni+=1
+            nj=0
+        nb.grid(row=ni, column=nj,sticky="W",padx=5,pady=3)
     
 root = tk.Tk()
 root.configure(background="#f2f2f2")
@@ -440,85 +733,25 @@ sb4.grid(row=3,column=0,padx=10,pady=10)
 
 sb5= tk.Button(frame3,relief="raised",text="Remove Password",command=rem_password,bg="White",fg="Black")
 
+
+
+
 #reports
 frame4=tk.Frame(root,background="#f2f2f2",bd=4)
 frame4.configure(height=frame1["height"],width=frame1["width"])
 frame4.grid_propagate(0)
-
-frame41=tk.Frame(frame4,width=frame1["width"],height=267)
-frame41.grid(row=0,column=0)
-frame41.grid_propagate(0)
-
-frame42=tk.Frame(frame4,width=frame1["width"])
-frame42.grid(row=1,column=0,sticky="SW")
-
-f4title=tk.Label(frame41,text="Reports",font=('Comic Sans',16))
-f4title.grid(row=0,column=0,columnspan=2)
-conn = sqlite3.connect('C:\Users\ABDUL\Downloads\sqlite-tools-win32-x86-3240000\sqlite-tools-win32-x86-3240000\dbva.db')
-print "Opened database successfully";
-cursor = conn.execute("SELECT * from reports")
-
-i=1
-j=0
-
-def v_report(x):
-    print "report"
-    print x
-    
-
-def v_details(x):
-    print "details"
-    x=str(x)
-    viewdetails = conn.execute("select * from reports where repid="+x)
-    for row in viewdetails:
-        r1=str(row[1])
-        r2=str(row[3])
-        r3=str(row[4])
-        r4=str(row[5])
-        t="Title : "+r1+"\nIssue Date : "+r2+"\nDue Date : "+r3+"\nStatus : "+r4
-        print t
-        dlabel=tk.Label(frame42,text=t,justify="left")
-        dlabel.grid(row=0,column=0)
-    
-
-for row in cursor:
-    c = row[1]
-    b = tk.Label(frame41,relief="ridge", text=c,width=30,bg="white",padx=5,anchor="w")
-    b.grid(row=i, column=j,sticky="W",padx=5,pady=3)
-    report=tk.Button(frame41, text="report",bg="white", command = partial(v_report,row[0]))
-    report.grid(row=i,column=j+1,padx=5,pady=3)
-    detail=tk.Button(frame41, text="details",bg="white", command = partial(v_details,row[0]))
-    detail.grid(row=i,column=j+2,padx=5,pady=3)
-    i=i+1
 
 #notes
 frame5=tk.Frame(root,background="#f2f2f2",bd=4)
 frame5.configure(height=frame1["height"],width=frame1["width"])
 frame5.grid_propagate(0)
 
-f5title=tk.Label(frame5,text="Notes",font=('Comic Sans',16),anchor="center")
-f5title.grid(row=0,column=0,columnspan=3)
-ncursor = conn.execute("SELECT * from notes")
 
-ni=1
-nj=-1
-for row in ncursor:
-    nj+=1
-    if row[3]==1:
-        nc="Protected. Click to View"
-        nb = tk.Button(frame5,relief="ridge",wraplength=67,justify="left",text=nc,width=10,height=5,anchor="nw",bg="white") 
-    elif row[3]==0:    
-        nc = row[1]
-        nb = tk.Label(frame5,relief="ridge",wraplength=67,justify="left",text=nc,width=10,height=5,anchor="nw",bg="white")
-    if nj==3:
-        ni+=1
-        nj=0
-    nb.grid(row=ni, column=nj,sticky="W",padx=5,pady=3)
     
     
-    """nd=row[2]
-    ne = tk.Label(frame5,relief="ridge", text=nd,width=30,bg="white",padx=5,anchor="w")
-    ne.grid(row=i+1, column=j,sticky="W",padx=5,pady=3)"""
+"""nd=row[2]
+ne = tk.Label(frame5,relief="ridge", text=nd,width=30,bg="white",padx=5,anchor="w")
+ne.grid(row=i+1, column=j,sticky="W",padx=5,pady=3)"""
     
 
 
--- notetest.py
+++ notetest.py
@@ -1,34 +1,186 @@
 import re
 import datetime
+import speech_recognition as sr
+import pyttsx
+import sqlite3
 
-x = "make a note that i have class tomo at 830 pm"
+con = sqlite3.connect('C:\Users\ABDUL\Downloads\sqlite-tools-win32-x86-3240000\sqlite-tools-win32-x86-3240000\dbva.db')
+con.text_factory = str
+c = con.cursor()
+
+engine = pyttsx.init()
+engine.setProperty('rate', 120)
+r = sr.Recognizer()
+def sun():
+    try:
+        with sr.Microphone() as source:
+            print "listening"
+            #response.configure(text="I'm asleep")
+            audio = r.listen(source)
+            a = r.recognize_google(audio)
+            print "heard"
+            print a
+            x = str(a)
+    except:
+        pass
+    return x
+
+x="make a note that I have to write on 26"
+"""
+try:
+    with sr.Microphone() as source:
+        print "listening"
+        #response.configure(text="I'm asleep")
+        audio = r.listen(source)
+        a = r.recognize_google(audio)
+        print "heard"
+        print a
+        x = str(a)
+except:
+    print "error"
+    """
+
 item=[]
-that = re.search('that',x)
-if that:
-    that = that.span()
-    z1 = that[1]
-    that = x[z1+1:]
-   
-#print x
 
-at = re.search(' at',x)
-if at:
-    at = at.span()
-    z2 = at[1]
-    at = x[z2+1:]
+def what():
+    what = re.search('that',x)
+    if what==None:
+        what = re.search('to',x)
+    if what == None:
+        engine.say("What would you want to make note about?")
+        engine.runAndWait()
+        what = sun()
+        return what  
+    what = what.span()
+    z1 = what[1]
+    what = x[z1+1:]
+    return what
+
     
 
-to = re.search('to',x)
-if to:
-    to = to.span()
-    z3 = to[1]
-    to = x[z3+1:]
+   
+#print x
+def when():
+    on = re.search(' on',x)
+    if on != None:
+        on = on.span()
+        z1 = on[1]
+        at = re.search(' at',x)
+        after = re.search(' after',x)
+        if at == None:   
+            on = x[z1+1:]
+        elif at:
+            at = at.span()
+            z2 = at[1]
+            on = x[z1+1:z2-3]
+            print on
+        if after == None:   
+            on = x[z1+1:]
+        elif after:
+            after = after.span()
+            z2 = after[1]
+            on = x[z1+1:z2-6]
+            print on
+        if (on.isdigit()==True):
+            y = datetime.datetime.now().year
+            m = datetime.datetime.now().month
+            on = datetime.datetime.strptime(str(y)+"-"+str(m)+"-"+on,"%Y-%m-%d")
+            print on
+        else:
+            print "error"
+    if on == None:
+        on = datetime.date.today()
+    if re.search('tomorrow',x):
+        on = datetime.date.today() + datetime.timedelta(days=1)
+    
+    at = re.search(' at',x)
+    if at!=None:
+        at = at.span()
+        z2 = at[1]
+        at = x[z2+1:]
+     
+        at2 = re.sub(' a.m.','AM',at)
+    
+        at2 = re.sub(' p.m.','PM',at)
+
+        print at2
+        
+        at = datetime.datetime.strptime(at2,"%I:%M%p")
+        at.strftime("%H:%M:%S")
+        #print at
+        
+    elif (re.search('after',x)):
+        at = re.search('after',x)
+        at = at.span()
+        z2 = at[1]
+        at = x[z2+1:]
+        h = re.search('hours',at)
+        if h!=None:
+            h = h.span()
+            z3 = h[1]
+            print z3
+            h = at[:z3-5]
+            h = int(h)
+            at2 = datetime.datetime.now() + datetime.timedelta(hours=h)
+            format(at2,"%H:%M:%S")
+            """at2=str(at2)
+            at2 = at2[:19]
+            print at2"""
+        m = re.search('minutes',at)
+        if m!=None:
+            m = m.span()
+            z3 = m[1]
+            print z3
+            m = at[:z3-7]
+            if (re.search('hours',at)):
+                hi = re.search('hours',at)
+                hi = hi.span()
+                z4 = hi[1]
+                m = at[z4+1:z3-7]
+                m = int(m)
+                at3 = at2 + datetime.timedelta(minutes=m)
+                format(at3,"%H:%M:%S")
+                at3=str(at3)
+                at3 = at3[:19]
+                at = at3
+            else:
+                m = int(m)
+                at3 = datetime.datetime.now() + datetime.timedelta(minutes=m)
+                format(at3,"%H:%M:%S")
+                at3=str(at3)
+                at3 = at3[:19]
+                print at
+                at=at3
+    else:
+        at = datetime.datetime.strptime("00:00:00","%H:%M:%S")
+        at.strftime("%Y-%m-%d %H:%M:%S")
+        
+    at=str(at)
+    at = at[10:]
+    on = str(on)
+    on = on[:10]   
+    on = on + at
+    on = datetime.datetime.strptime(on,"%Y-%m-%d %H:%M:%S")
+    return on.strftime("%Y-%m-%d %H:%M:%S")
+    
+
+
     
         
 datecreated=datetime.datetime.now()
-item.insert(0,that)
-item.insert(1,datecreated.strftime("%Y-%m-%d %H:%M:%S"))
-item.insert(2,at)
+item.insert(0,what())
+item.insert(1,when())
+#item.insert(2,when())
+item.insert(2,'0')
 
 print item
+t=[6,'note 6',datetime.datetime.now(),0]
+c.execute("insert into notes(text,date_created,locked) values(?,?,?)",item)
+con.commit()
+#con.close()
+print "enetered"
 
+c=con.cursor()
+vnote= c.execute("select text from notes where nid=2")
+for row in vnote:
+    print row[0]
